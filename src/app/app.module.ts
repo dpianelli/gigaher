792bbf7bbd403dcbdf80fb19be6204b1eabe887a
ddpiane1 <dominique.pianelli@hobbylobby.com>
2018-03-06 11:05:37 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+// Service
+import { DataService } from './data.service';
+
+// Pages Components
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA],
+})
+export class AppModule { }
